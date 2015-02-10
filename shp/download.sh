1eadba587ff3d3c4007ad4c46ea30f7c893a739a
sgiehl <stefan@piwik.org>
2015-02-10 21:33:06 +0100

    fixed filenames

--- shp/download.sh
+++ shp/download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/50m-admin-0-countries.zip
-unzip -d . 50m-admin-0-countries.zip
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/physical/50m-lakes.zip
-unzip -d . 50m-lakes.zip
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m-admin-1-states-provinces-shp.zip
-unzip -d . 10m-admin-1-states-provinces-shp.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip
+unzip -d . ne_50m_admin_0_countries.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/physical/ne_50m_lakes.zip
+unzip -d . ne_50m_lakes.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip
+unzip -d . ne_10m_admin_1_states_provinces.zip
